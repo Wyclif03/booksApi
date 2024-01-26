4e200044be2d8d8e43e3f22febe1659c436a88e8
d8a0 <151666577+d8a0@users.noreply.github.com>
2024-01-26 10:27:13 +0000

    final version

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,15 +1,18 @@
 const createError = require('http-errors');
+const Book = require("../models/books");
 
-let books = [
-    {title: 'The Lord of the Rings', author: 'J.R.R. Tolkien', read: true, id: 1}, 
-    {title: 'Harry Potter and the Philosopher\'s Stone', author: 'J.K. Rowling', read: false, id: 2}, 
-    {title: 'The Hobbit', author: 'J.R.R. Tolkien', read: true, id: 3}, 
-    ];
+
 
 // SHOW ALL BOOKS
-exports.showBooks = (req, res, next) => {
-    res.send(books)
+exports.showBooks = async function (req, res) {
+    try {
+        const books = await Book.find();
+        res.send(books)
+    } catch (err) {
+        return next(createError(500, err.message))
+    }
 }
+
 
 // ADD A BOOK
 exports.addBook = (req, res, next) => {
@@ -22,73 +25,75 @@ exports.addBook = (req, res, next) => {
     const newBook = req.body;
 
     // ASSIGN A UNIQUE ID
-    req.body.id = books.length + 1 +Date.now()
+    req.body.id = books.length + 1 + Date.now()
 
     // Validate the 'read' property to ensure it's a boolean value
     if (typeof newBook.read !== 'boolean') {
         return next(createError(400, 'Invalid value for the "read" property. It should be a boolean.'));
     }
-        // if all true add book to books array + return array to user / postman
-       books.push(req.body)
-        res.send(books)    
-}
-
-//REMOVE A BOOK
-exports.removeBook = (req, res, next) => {
-    console.log("removeBook function")
-    books.pop()
-    res.send({message: "Book has been removed", books: books})
+    // if all true add book to books array + return array to user / postman
+    books.push(req.body)
+    res.send(books)
 }
 
 // UPDATE -EDIT A BOOK
-exports.editBook = (req, res, next) => {
-    const updatedBook = req.body; // Get the updated book data from the request body
-    let bookFound = false; // if the book is found
-    // Iterate over the books array
-    books.forEach((book, index) => {
-        // If the current book's id matches the updated book's id, update the book in the array
-        if (book.id === updatedBook.id) {
-            books[index] = Object.assign(book, updatedBook);
-            bookFound = true;
+exports.editBook = async function (req, res, next) {
+    try {
+        const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedBook) {
+            return next(createError(404, "cannot find book by ID"));
         }
-        
-    });
-    if (!bookFound) {
-        next(createError(404, 'Book not found'));
-    } else {
-        res.send({message: "Book updated", books: books});
+        res.send(updatedBook);
+    } catch (err) {
+        return next(createError(500, err.message));
     }
 }
+
+
 //RETURN BOOK BY ID
-exports.getBookById = (req, res, next) => {
-    const bookId = parseInt(req.params.id);
-    if (isNaN(bookId)) {
-        return next(createError(400, 'Invalid book ID'));
+exports.getBookById = async function (req, res, next) {
+    try {
+        const correctIdBook = await Book.findById(req.params.id);
+        if (!correctIdBook) {
+            return next(createError(404, "no book with that id"));
+        }
+        res.send(correctIdBook);
+    } catch (err) {
+        return next(createError(500, err.message));
     }
-    
-    // Match request with book ID's     
-    const foundBook = books.find(book => book.id === bookId);
+};
 
 
-    // Error if not found
-    if (!foundBook) {
-        return next(createError(404, 'Book not found'));
-    }
-
-// Return book
-    res.send(foundBook);
-}
 
 //DELETE BOOK BY ID
-exports.deleteBookById = (req, res, next) => {
-        const bookId = parseInt(req.params.id);
-        if (isNaN(bookId)) {
-            return next(createError(400, 'Invalid book ID'));
-        } 
-        
-        const index = books.findIndex(book => book.id === bookId);
-        books.splice(index, 1)
 
-    
-    res.send(books)
-}
+exports.deleteBookById = async function (req, res, next) {
+    try {
+        const bookItemToDelete = await Book.findByIdAndDelete(req.params.id);
+        if (!bookItemToDelete) {
+            return next(createError(404, "no book with that id"));
+        }
+        res.send({ result: true });
+    } catch (err) {
+        return next(createError(500, err.message));
+    }
+};
+
+
+// ADD BOOK TO MONGODB
+
+exports.addBookToDb = async function (req, res, next) {
+    try {
+        if (!req.body.title) {
+            return next(createError(400, "title is required"));
+        }
+        const bookItem = new Book({
+            title: req.body.title,
+        });
+        await bookItem.save();
+        res.send(bookItem);
+    } catch (err) {
+        return next(createError(500, err.message));
+    }
+};
+
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {showBooks, addBook, removeBook, editBook, getBookById, deleteBookById} = require('../controllers/bookController');
+const { showBooks, editBook, getBookById, deleteBookById, addBookToDb } = require('../controllers/bookController');
 
 
 router.get('/showBooks', showBooks);
 
-router.post('/addBook', addBook)
-
-router.delete('/removeBook', removeBook)
-
-router.put('/editBook', editBook)
+router.put('/editBookById/:id', editBook)
 
 router.get('/getBookById/:id', getBookById)
 
 router.delete('/deleteBookById/:id', deleteBookById)
+
+router.post('/addBookToDb', addBookToDb)
 
 module.exports = router;
